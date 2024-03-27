7c1a09587e8c092b8a6a7f03c703cd33b9c7b5a9
adamli008 <adamli008@gmail.com>
2024-03-26 20:31:18 -0700

    Initial Commit

---
+++ Hello World/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by mdsadmin on 2024/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
